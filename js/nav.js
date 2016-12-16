19912aafb3458ea7de8958c773f5547b2949159d
sebahub <annika@Jonas-iMac.local>
2016-12-16 11:38:30 +0100

    Nav works now
    superduper

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,6 @@
- 
+<!-- Topbar resize -->
+
+
 $(function() {
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
@@ -10,3 +12,27 @@ $(function() {
     });
 });
 
+
+<!-- Burgermenü -->
+
+$(document).ready(function(){
+    $("#nav_mobile").click(function(){
+        $("#button").trigger("click");
+    });
+});
+
+<!-- Zeit vorbei -->
+    
+$('#timeTable tr td').each(function () {
+    var dtSt = $(this).html().split(" ");
+    var dtAr = dtSt[0].split("/");
+    var when = new Date(dtAr[1] + "/" + dtAr[0] + "/" + dtAr[2] + " " + dtSt[1]);
+    console.log(when);    
+    var now = Date.now();
+    
+    if (now - when > 3600000) {
+        $(this).parent('tr').addClass('min60');
+    } else if (now - when > 1800000) {
+        $(this).parent('tr').addClass('min30');
+    }
+});
